96b73c52f241c83bdaa76a24e008bb1be1b99a1c
jeet <jeetbasak54@gmail.com>
2021-07-25 13:24:36 +0530

    jeet

--- public/admin/assets/js/scripts.js
+++ public/admin/assets/js/scripts.js
@@ -18,32 +18,107 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 // Input Type Number JS
-document.addEventListener('DOMContentLoaded', function() {
-	var inputs = document.getElementsByClassName('product-dtl-quantity')
-	function incInputNumber(input, step) {
-		var val = +input.value
-		if (isNaN(val)) val = 1
-		val += step
-		input.value = val > 1 ? val: 1
-	}
-	Array.prototype.forEach.call(inputs, function(el) {
-		var input = el.getElementsByTagName('input')[0]
-		
-		el.getElementsByClassName('increase')[0].addEventListener('click', function() { incInputNumber(input, 1) })		
-	})
-})
-// Input Type Number JS
-document.addEventListener('DOMContentLoaded', function() {
-	var inputs = document.getElementsByClassName('product-dt2-quantity')
-	function incInputNumber(input, step) {
-		var val = -input.value
-		if (isNaN(val)) val = 100
-		val += step
-		input.value = val > 1 ? val: 1
-	}
-	Array.prototype.forEach.call(inputs, function(el) {
-		var input = el.getElementsByTagName('input')[0]
-		
-		el.getElementsByClassName('decrease')[0].addEventListener('click', function() { incInputNumber(input, 1) })
-	})
-})
+$('.minus').click(function () {
+	var $input = $(this).parent().find('input');
+	var count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.plus').click(function () {
+	var $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
+
+
+
+$(document).ready(function(){
+
+    var current_fs, next_fs, previous_fs;
+    
+    // No BACK button on first screen
+    if($(".show").hasClass("first-screen")) {
+    $(".prev").css({ 'display' : 'none' });
+    }
+    
+    // Next button
+    $(".next-button").click(function(){
+    
+    current_fs = $(this).parent().parent();
+    next_fs = $(this).parent().parent().next();
+    
+    $(".prev").css({ 'display' : 'block' });
+    
+    $(current_fs).removeClass("show");
+    $(next_fs).addClass("show");
+    
+    $("#progressbar li").eq($(".card2").index(next_fs)).addClass("active");
+    
+    current_fs.animate({}, {
+    step: function() {
+    
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    
+    next_fs.css({
+    'display': 'block'
+    });
+    }
+    });
+    });
+    
+    // Previous button
+    $(".prev").click(function(){
+    
+    current_fs = $(".show");
+    previous_fs = $(".show").prev();
+    
+    $(current_fs).removeClass("show");
+    $(previous_fs).addClass("show");
+    
+    $(".prev").css({ 'display' : 'block' });
+    
+    if($(".show").hasClass("first-screen")) {
+    $(".prev").css({ 'display' : 'none' });
+    }
+    
+    $("#progressbar li").eq($(".card2").index(current_fs)).removeClass("active");
+    
+    current_fs.animate({}, {
+    step: function() {
+    
+    current_fs.css({
+    'display': 'none',
+    'position': 'relative'
+    });
+    
+    previous_fs.css({
+    'display': 'block'
+    });
+    }
+    });
+    });
+    
+    });
+
+
+
+    $(document).ready(function() {
+        $("#show_hide_password a").on('click', function(event) {
+            event.preventDefault();
+            if($('#show_hide_password input').attr("type") == "text"){
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass( "fa-eye-slash" );
+                $('#show_hide_password i').removeClass( "fa-eye" );
+            }else if($('#show_hide_password input').attr("type") == "password"){
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass( "fa-eye-slash" );
+                $('#show_hide_password i').addClass( "fa-eye" );
+            }
+        });
+    });

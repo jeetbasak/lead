e75269d0271d72da98c32c32cc55c19399fc9421
jeet <jeetbasak54@gmail.com>
2021-07-30 17:58:33 +0530

    jeet

--- app/Http/Controllers/Frontend/Modules/Faq/FaqController.php
+++ app/Http/Controllers/Frontend/Modules/Faq/FaqController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Frontend\Modules\Faq;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Faq;
+
 
 class FaqController extends Controller
 {
-    //
+    
+
+    public function faq_list(Request $request){
+		$data['faq']=Faq::where('status','A')->get();
+		return view('frontend.modules.faq.faq')->with($data);
+	}
 }
--- resources/views/frontend/modules/faq/faq.blade.php
+++ resources/views/frontend/modules/faq/faq.blade.php
@@ -1,9 +1,9 @@
-@extends('admin.layouts.app')
+@extends('layouts.app')
 @section('title')
-<title>Admin | Faq list</title>
+<title>User | Faq list</title>
 @endsection
 @section('left_part')
-@include('admin.include.left_part')
+@include('frontend.include.left_part')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -12,19 +12,11 @@
 @section('content')
 <!-- Start right Content here -->
 <!-- ============================================================== -->
-@include('admin.include.errors')
+@include('frontend.include.errors')
 <div class="container-fluid">
 	<div class="body-main">
 		<div class="top-row">
-			<div class="task-mg-row b-b-n">
-				<div class="dropdown">
-					<a href="{{route('faq.add.form')}}" class="add-btn dropdown-toggle" style=" color: white; "><span class="plus">+</span> <div class="br-r">Add Faq</div>
-				</a>
-				<span class="caret"></span></button>
-			</div>
-			
-			
-		</div>
+		
 	</div>
 	<div class="top-row">
 		<table id="example">
@@ -52,66 +44,19 @@
 					@endif</td>
 					
 					<td data-label="Action">
-						{{--
-						<a href="#" type="button"  data-toggle="modal" data-target="#myModalressing{{@$value->id}}" style="margin-left: 5px;"><i class="fa add-round">&#8592;</i></a>
-						--}}
-						<a href="#" type="button"  data-toggle="modal" data-target="#myModaledit{{@$value->id}}" style="margin-left: 5px;"><i class="fa fa-edit edit-round"></i></a>
-						<a onclick="return confirm('Are you sure want to delete this faq?');" href="{{route('faq.dlt',$value->id)}}" style="margin-left: 5px;"><i class="fa fa-trash-o del-round"></i></a>
+					
 						<a href="#" type="button"  data-toggle="modal" data-target="#myModalview{{@$value->id}}" style="margin-left: 5px; font-size:25px"><i class="fa fa-eye" aria-hidden="true"></i></a>
 						
 					</td>
 				</tr>
-				{{-- for edit --}}
-				<div class="modal" id="myModaledit{{@$value->id}}">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<!-- Modal Header -->
-							<div class="modal-header">
-								<h4 class="modal-title">Edit faq</h4>
-								
-							</div>
-							<!-- Modal body -->
-							<div class="modal-body">
-								
-								<form method="post" action="{{route('update.faq')}}">
-									@csrf
-									<input type="hidden" name="faq_id" value="{{@$value->id}}">
-									<div class="form-group">
-										<label for="search">Select to assing</label>
-										<textarea id="qus" name="qus">{{@$value->question}}</textarea>
-										<textarea id="ans" name="ans">{{@$value->answer}}</textarea>
-
-										<select class="form-control custom_select" id
-                                    ="status" name="status">
-                                      <option @if(@$value->status=="A") selected @endif value="A">Active</option>
-                                      <option @if(@$value->status=="I") selected @endif value="I">Deactive</option>
-                                                                            
-                                    </select>
-									</div>
-									<input type="submit" class="btn btn-primary mb-2" value="submit">
-									
-								</form>
-								
-								
-							</div>
-							<!-- Modal footer -->
-							<div class="modal-footer">
-								
-								
-								<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				{{-- for ressing --}}
-				
+			
 				{{-- for view --}}
 				<div class="modal" id="myModalview{{@$value->id}}">
 					<div class="modal-dialog">
 						<div class="modal-content">
 							<!-- Modal Header -->
 							<div class="modal-header">
-								<h4 class="modal-title">Details of faq no {{@$value->id}}</h4>
+								<h4 class="modal-title">Faq details</h4>
 								
 							</div>
 							<!-- Modal body -->
@@ -152,12 +97,12 @@
 <!-- ============================================================== -->
 <!-- End Right content here -->
 {{-- @section('footer')
-@include('admin.include.footer')
+@include('frontend.include.footer')
 @endsection --}}
 @endsection
 {{-- end content --}}
 @section('script')
-@include('admin.include.script')
+@include('frontend.include.script')
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
